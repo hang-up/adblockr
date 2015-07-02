577262d4c2763b6906cc5065ffd455a32531ec5c
Bob <roberthang@hotmail.fr>
2015-07-02 01:04:00 -0400

    First refactor jQuery-less (closing yet to be done)

--- js/adblockr.js
+++ js/adblockr.js
@@ -8,38 +8,80 @@ var blockrContainer = {
 
     setBackgroundColor : function() {
         var index = Math.floor(Math.random()*blockrContainer.options.colors.length);
-        $(".adblocker-overlay").css("background-color", blockrContainer.options.colors[index]);
+        document.getElementsByClassName("adblocker-overlay")[0].style.backgroundColor = blockrContainer.options.colors[index];
     },
 
-    getText: "<div class='adblocker-overlay'>" +
+    getText: function() {
+        return blockrContainer.buildDomElement("<div class='adblocker-overlay'>" +
         "<span class='close-cross' data-target='adblocker-overlay'></span>" +
-        "<h1 id='adblocker-title'> <img width='100' height='100' src='palm1.svg' id='adblocker-icon'><br></h1> " +
-        "<p id='adblocker-caption'></p>" +
-        "</div>",
+        "<h1 class='center'><img width='100' height='100' src='palm1.svg' id='adblocker-icon'></h1>" +
+        "<h1 class='center' id='adblocker-title'></h1> " +
+        "<p class='center' id='adblocker-caption'></p>" +
+        "</div>")
+    },
 
     appendText : function() {
-        if (document.getElementById("ad-blockr") === undefined) {
-            $("body").hide().append(blockrContainer.getText).fadeIn();
-            $("#adblocker-title").append(blockrContainer.options.title);
-            $("#adblocker-caption").text(blockrContainer.options.caption);
+        if (!document.getElementById("ad-blockr")) {
+            //Cache the body.
+            var body = document.getElementsByTagName("body")[0];
+
+            //Append main text to the body.
+            body.style.display = "none";
+            body.appendChild(blockrContainer.getText());
+
+            //Append title and captions.
+            document.getElementById("adblocker-title").innerHTML = blockrContainer.options.title;
+            document.getElementById("adblocker-caption").innerHTML = blockrContainer.options.caption;
+
+            //Fade in the container.
+            blockrContainer.fadeIn(body);
         }
     },
 
     closePanel : function() {
-        $(".close-cross").on("click", function() {
-            if ($(this).data("target") === "adblocker-overlay") {
-                $(".adblocker-overlay").fadeOut(300, function() {
-                    $(this).remove();
-                });
-            }
-        })
+        document.querySelector(".adblocker-overlay .close-cross").onclick = function() {
+            alert("hi");
+        };
+
+//        $(".close-cross").on("click", function() {
+//            if ($(this).data("target") === "adblocker-overlay") {
+//                $(".adblocker-overlay").fadeOut(300, function() {
+//                    $(this).remove();
+//                });
+//            }
+//        })
     },
 
     aggressive : function(value) {
         if(!value) {
-            $(".adblocker-overlay .close-cross").show();
+            document.querySelector(".adblocker-overlay .close-cross").style.display = "block";
             blockrContainer.closePanel();
         }
+    },
+
+    fadeIn: function(element) {
+        var op = 0.1;  // initial opacity
+        element.style.display = 'block';
+        var timer = setInterval(function () {
+            if (op >= 1){
+                clearInterval(timer);
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op += op * 0.1;
+        }, 10);
+    },
+
+
+    //http://stackoverflow.com/a/814649
+    buildDomElement: function (htmlStr) {
+        var frag = document.createDocumentFragment(),
+            temp = document.createElement('div');
+        temp.innerHTML = htmlStr;
+        while (temp.firstChild) {
+            frag.appendChild(temp.firstChild);
+        }
+        return frag;
     },
 
     init: function(options) {
